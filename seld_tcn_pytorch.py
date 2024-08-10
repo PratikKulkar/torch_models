cb1e45d780f4537159533bd95ab13f6d0e221e1c
PratikKulkar <kulkarpratik@gmail.com>
2024-08-10 23:08:54 +0530

    removal of unecessory libraries

--- seld_tcn_pytorch.py
+++ seld_tcn_pytorch.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import nobuco
-from nobuco import ChannelOrder, ChannelOrderingStrategy
 
 class SELDTCNModel(nn.Module):
     def __init__(self, 
